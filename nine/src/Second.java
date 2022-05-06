18b89e01f7e08ae04571a852586ddd43d5eba479
Artemiy Sartakov <artemiy.sartakov@jetbrains.com>
2022-05-06 15:20:45 +0700

    9: second answers

--- nine/src/Second.java
+++ nine/src/Second.java
@@ -1,5 +1,7 @@
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Second {
 
@@ -20,36 +22,58 @@ public class Second {
     // 1. create map Worker -> jobTitle for all departments in firm
 
     public static Map<Worker, String> jobTitles(Firm firm) {
-        return null;
+        return firm.departments().stream()
+                .flatMap(d -> d.workers().stream())
+                .collect(Collectors.toMap(w -> w, Worker::jobTitle));
     }
 
     // 2. calculate total salary for given department
 
     public int departmentSalary(Department department) {
-        return 0;
+        return department.workers().stream()
+                .mapToInt(Worker::salary)
+                .sum();
     }
 
     // 3. find any department with more than n workers
 
     Department bigDepartment(Firm firm, int n) {
-        return null;
+        return firm.departments().stream()
+                .filter(d -> d.workers().size() > n)
+                .findAny()
+                .orElse(null);
     }
 
     // 4. create list of workers' job titles with the smallest salary in each department.
     // format: [ JOB_1, JOB_2, ..., JOB_N ]
 
     private static String cheapProfessions(Firm firm) {
-        return null;
+        return firm.departments().stream()
+                .map(d -> cheapestWorker(d.workers()))
+                .map(Worker::jobTitle)
+                .collect(Collectors.joining(", ", "[ ", " ]"));
     }
 
     // you can use this auxiliary method if needed
     private static Worker cheapestWorker(List<Worker> workers) {
-        return null;
+        class WorkerComparator implements Comparator<Worker> {
+
+            @Override
+            public int compare(Worker o1, Worker o2) {
+                return Integer.compare(o1.salary(), o2.salary());
+            }
+        }
+        return workers.stream()
+                .min(new WorkerComparator())
+                .get();
     }
 
     // 5. get three most resent departments
 
-    Department[] recentDepartments(Firm firm) {
-        return null;
+    private static Department[] recentDepartments(Firm firm) {
+        return firm.departments().stream()
+                .sorted(Comparator.comparingLong(Department::createdAt).reversed())
+                .limit(3)
+                .toArray(Department[]::new);
     }
 }
